21208b9f755e1e8e4ff2edacf327eb79d24ecd0b
JaeHunRo <jhr24@duke.edu>
2015-08-01 18:25:30 -0700

    testing env

---
+++ mytest/index.js
@@ -0,0 +1,18 @@
+console.log(process.env);
+
+// var sendgrid = require('sendgrid')('azai91', 'AUfsLAayVXpKZamD2ZC4');
+// var fs = require('fs');
+
+// var html = "<html><head><style>.error {color: #c00;font-size: 1em;font-weight: 100;letter-spacing: 1px;}</style></head><body>" + fs.readFileSync(__dirname + '/output.html').toString() + "</body></html>";
+
+// var config = require('config');
+// var email = config.get('email');
+
+// if (email){
+// 	sendgrid.send({
+// 	      to: email,
+// 	      from: 'hello@codesmith.io',
+// 	      subject: 'Hack Hour Result',
+// 	      html: html
+// 	});
+// }
