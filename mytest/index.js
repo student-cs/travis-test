630624529c1e2c51e6ead597b8fef8e2608a60a4
JaeHunRo <jhr24@duke.edu>
2015-08-03 11:20:10 -0700

    changed request

--- mytest/index.js
+++ mytest/index.js
@@ -6,12 +6,9 @@ var http = require('http');
 
 var commit_number = process.env.TRAVIS_COMMIT;
 var access_token = 'a91e3e29f5d0d391415a6f403c830a323051ad9f';
-var path = '/repos/JaeHunRo/travis-test/commits/' + commit_number + '?access_token=' + access_token;
+var url = 'https://api.github.com/repos/JaeHunRo/travis-test/commits/' + commit_number + '?access_token=' + access_token;
 http.get(
-	{
-		host: 'www.api.github.com',
-		path: path
-	}, 
+	url, 
 	function(response){
 		var string = '';
 		response.on('data', function(d){
