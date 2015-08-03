ea8176ce358e59a9ad11b40ebd6b646e143143ce
JaeHunRo <jhr24@duke.edu>
2015-08-03 12:01:13 -0700

    https headers and access

--- mytest/index.js
+++ mytest/index.js
@@ -5,10 +5,18 @@ var https = require('https');
 // var html = "<html><head><style>.error {color: #c00;font-size: 1em;font-weight: 100;letter-spacing: 1px;}</style></head><body>" + fs.readFileSync(__dirname + '/output.html').toString() + "</body></html>";
 
 var commit_number = process.env.TRAVIS_COMMIT;
-var access_token = 'a91e3e29f5d0d391415a6f403c830a323051ad9f';
-var url = 'https://api.github.com/repos/JaeHunRo/travis-test/commits/' + commit_number + '?access_token=' + access_token;
-https.get(
-	url, 
+var access_token = 'e65672b1a90d6725aa57e4723830fd759437a0f1';
+var path = '/repos/JaeHunRo/travis-test/commits/' + commit_number + '?access_token=' + access_token;
+var options = {
+	host: 'api.github.com',
+	path: path,
+	method: 'GET',
+	headers: {
+		'user-agent': 'node.js'
+	}
+};
+https.request(
+	options, 
 	function(response){
 		var string = '';
 		response.on('data', function(d){
@@ -18,7 +26,7 @@ https.get(
 			var email = JSON.parse(string).commit.committer.email;
 			console.log(email, 'should be email');
 		});
-	});
+	}).end();
 process.on('uncaughtException', function (err) {
     console.log(err);
 }); 
